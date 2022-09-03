09b23384ddea41387604a5b93b0fa9d76cf0aa4d
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2022-09-02 20:58:39 -0400

    commit

---
+++ Week 4/langs/languages.js
@@ -0,0 +1,31 @@
+const lang = 'JavaScript'
+
+// Create an event listener for the submit button that adds all 'input' elements to 
+// a single array using the spread operator. Call the chkLang() function, passing in 
+// the array as an argument.
+
+document.querySelector("#submit").addEventListener("click", () =>{
+    let input = document.querySelectorAll("input");
+    let inputBox = [...input];
+    chkLang(inputBox);
+})
+
+
+function chkLang(langs) {
+    let result = false
+
+    langs.forEach((inputLang) => {
+        if (inputLang.value === lang) {
+            result = true;
+        }
+    });
+    
+    // use an array method to check whether the user included 'JavaScript' in their
+    // list of languages
+
+    let obj = document.querySelector('#TestResult')
+    if (result) 
+        obj.innerText = `Congratulations!\nYou know ${lang}.`
+    else
+        obj.innerText = `Sorry,\nyou don't know ${lang}.`
+}
